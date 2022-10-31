6f7c71925418ee565ed9880f36c7a2d35740491f
Brian Degger <brian.degger@gmail.com>
2022-10-31 00:49:12 +0000

    Add files via upload
    Secrets file to rename and update.

---
+++ secretsRename.py
@@ -0,0 +1,14 @@
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
+# This file is where you keep secret settings, passwords, and tokens!
+# If you put them in the code you risk committing that info or sharing it
+
+secrets = {
+    "ssid": "xxx", #Router's (AP) name
+    "password": "yyyy", #Router's (AP) password
+    "timezone": 0,  # this is offset from UTC
+    "github_token": "abcdefghij0123456789", 
+    "aio_username": "aaaa", #user name for adafruit io
+    "aio_key": "bbbb", #key for adafruit io
+}
